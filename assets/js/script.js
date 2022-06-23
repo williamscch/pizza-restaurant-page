877cae84a1988c3386b1119414404fcec3fab4c3
wdavidcch <david.13.2121@gmail.com>
2022-06-23 13:37:38 -0400

    Mobile menu for home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,32 +91,71 @@ for (let i = 0; i < popularPizzasObj.length; i += 1) {
 
 // mobile menu
 
-const button = document.querySelector('.hamburger-button');
+const button = document.querySelector('.menu-btn');
+let menuOpen = false;
 const menu = document.querySelector('.navbar-items');
 const body = document.querySelector('body');
-
-// const closer = document.querySelector('.x');
-// const closePortfolio = document.querySelector('.link');
-// const closeAbout = document.querySelector('.link2');
-// const closeContact = document.querySelector('.link3');
+const closeDeals = document.querySelector('.deals-menu');
+const closeMenu = document.querySelector('.menu-menu');
+const closeLocation = document.querySelector('.location-menu');
+const closeContact = document.querySelector('.contact-menu');
 
 button.addEventListener('click', () => {
+  if (!menuOpen) {
+    button.classList.add('open');
+    menuOpen = true;
+  } else {
+    button.classList.remove('open');
+    menuOpen = false;
+  }
   menu.classList.toggle('active');
   body.classList.toggle('active');
 });
 
-// closer.addEventListener('click', () => {
-//   menu.classList.toggle('active');
-// });
+closeDeals.addEventListener('click', () => {
+  if (!menuOpen) {
+    button.classList.add('open');
+    menuOpen = true;
+  } else {
+    button.classList.remove('open');
+    menuOpen = false;
+  }
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
+});
 
-// closePortfolio.addEventListener('click', () => {
-//   menu.classList.toggle('active');
-// });
+closeMenu.addEventListener('click', () => {
+  if (!menuOpen) {
+    button.classList.add('open');
+    menuOpen = true;
+  } else {
+    button.classList.remove('open');
+    menuOpen = false;
+  }
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
+});
 
-// closeAbout.addEventListener('click', () => {
-//   menu.classList.toggle('active');
-// });
+closeLocation.addEventListener('click', () => {
+  if (!menuOpen) {
+    button.classList.add('open');
+    menuOpen = true;
+  } else {
+    button.classList.remove('open');
+    menuOpen = false;
+  }
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
+});
 
-// closeContact.addEventListener('click', () => {
-//   menu.classList.toggle('active');
-// });
+closeContact.addEventListener('click', () => {
+  if (!menuOpen) {
+    button.classList.add('open');
+    menuOpen = true;
+  } else {
+    button.classList.remove('open');
+    menuOpen = false;
+  }
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
+});
